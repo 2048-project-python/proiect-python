88ecbfe7e9703e917f8470e14e99a2262d054d6e
Felicia Gorgos <feliciagorgos@gmail.com>
2022-05-15 21:15:26 +0300

    aproape gata

--- proiect.py
+++ proiect.py
@@ -35,6 +35,8 @@ class Game:
             self.previous.append(self.grid[i].copy())
         self.highscore = 0
         self.moves = 0
+        self.in_game = 1 # change when game starts
+        self.status = 0 # 0 - game has not ended yet, 1 - winner, -1 - failure
 
     def run(self):
         running = True
@@ -109,18 +111,42 @@ class Game:
         f.close()
 
     def draw(self):
-        self.window.fill(GREY)
-        #self.displayTable()
-        for i in range(4):
-            for j in range(4):
-                tile = pygame.Rect(100 + j * 100, 200 + i * 100, 100, 100)
-                pygame.draw.rect(self.window,ANOTHERGREY,tile,100)
-                self.printTile((j, i))
-        tile = pygame.Rect(96, 196, 408, 408)
-        pygame.draw.rect(self.window,ANOTHERGREY,tile,4)
-        self.printScore()
-        self.printHighScore()
-        self.printMoves()
+        if self.status == 0:
+            self.window.fill(GREY)
+            #self.displayTable()
+            for i in range(4):
+                for j in range(4):
+                    tile = pygame.Rect(100 + j * 100, 200 + i * 100, 100, 100)
+                    pygame.draw.rect(self.window,ANOTHERGREY,tile,100)
+                    self.printTile((j, i))
+            tile = pygame.Rect(96, 196, 408, 408)
+            pygame.draw.rect(self.window,ANOTHERGREY,tile,4)
+
+            self.printScore()
+            self.printHighScore()
+            self.printMoves()
+
+            undo_rect = pygame.Rect(418, 135, 30, 30)
+            undo_image = pygame.image.load("C:/Users/Felicia/python/proiect-python/images/undo.png")
+            undo_image = pygame.transform.scale(undo_image, (30, 30))
+            self.window.blit(undo_image, undo_rect)
+
+            replay_rect = pygame.Rect(460, 135, 30, 30)
+            replay_image = pygame.image.load("C:/Users/Felicia/python/proiect-python/images/replay.png")
+            replay_image = pygame.transform.scale(replay_image, (30, 30))
+            self.window.blit(replay_image, replay_rect)
+
+
+        elif self.status == -1:
+            lost_rect = pygame.Rect(96, 196, 408, 408)
+            lost_image = pygame.image.load("C:/Users/Felicia/python/proiect-python/images/lost.png")
+            lost_image = pygame.transform.scale(lost_image, (408, 408))
+            self.window.blit(lost_image, lost_rect)
+        elif self.status == 1:
+            won_rect = pygame.Rect(96, 196, 408, 408)
+            won_image = pygame.image.load("C:/Users/Felicia/python/proiect-python/images/won.png")
+            won_image = pygame.transform.scale(won_image, (408, 408))
+            self.window.blit(won_image, won_rect)
         
 
     def addTile(self):
@@ -148,7 +174,7 @@ class Game:
         self.window.blit(image, (position[0]-100+2,position[1]+100+2))
 
     def getPath(self, value):
-        return "C:/Users/User/Desktop/2048_Py/Python proiect/images/image_2048_" + str(value) + ".png"
+        return "C:/Users/Felicia/python/proiect-python/images/image_2048_" + str(value) + ".png"
 
     def rot90(self, grid):
         new_matrix = [[0 for _ in range(4)] for _ in range(4)]
@@ -189,20 +215,74 @@ class Game:
                 return (False, alreadyFused)
         return (False, alreadyFused)
 
-    def canMove(self, position, alreadyFused):
-        if position[1] > 2 or position[0] > 3:
-            return 0
-        if position[1] < 0 or position[0] < 0:
-            return 0
-        if self.grid[position[0]][position[1]] == 0:
-            return 0
-        if self.grid[position[0]][position[1] + 1] == 0:
-            return 2
-        if self.grid[position[0]][position[1]] == self.grid[position[0]][position[1] + 1]:
-            if not alreadyFused:
+    def canMove(self, position, alreadyFused, direction = 'Q'):
+        if direction == 'Q':
+            if self.canMove(position, alreadyFused, 'W') or self.canMove(position, alreadyFused, 'S') or self.canMove(position, alreadyFused, 'A') or self.canMove(position, alreadyFused, 'D'):
                 return 1
             else:
                 return 0
+        elif direction == 'W':
+            if position[1] > 3 or position[0] > 3:
+                return 0
+            if position[1] < 0 or position[0] < 1:
+                return 0
+            if self.grid[position[0]][position[1]] == 0:
+                return 0
+            if self.grid[position[0] - 1][position[1]] == 0:
+                return 2
+            if self.grid[position[0]][position[1]] == self.grid[position[0] - 1][position[1]]:
+                if not alreadyFused:
+                    return 1
+                else:
+                    return 0
+
+        elif direction == 'S':
+            if position[1] > 3 or position[0] > 2:
+                return 0
+            if position[1] < 0 or position[0] < 0:
+                return 0
+            if self.grid[position[0]][position[1]] == 0:
+                return 0
+            if self.grid[position[0] + 1][position[1]] == 0:
+                return 2
+            if self.grid[position[0]][position[1]] == self.grid[position[0] + 1][position[1]]:
+                if not alreadyFused:
+                    return 1
+                else:
+                    return 0
+            
+        elif direction == 'A':
+            if position[1] > 3 or position[0] > 3:
+                return 0
+            if position[1] < 1 or position[0] < 0:
+                return 0
+            if self.grid[position[0]][position[1]] == 0:
+                return 0
+            if self.grid[position[0]][position[1] - 1] == 0:
+                return 2
+            if self.grid[position[0]][position[1]] == self.grid[position[0]][position[1] - 1]:
+                if not alreadyFused:
+                    return 1
+                else:
+                    return 0
+
+        elif direction == 'D':
+            if position[1] > 2 or position[0] > 3:
+                return 0
+            if position[1] < 0 or position[0] < 0:
+                return 0
+            if self.grid[position[0]][position[1]] == 0:
+                return 0
+            if self.grid[position[0]][position[1] + 1] == 0:
+                return 2
+            if self.grid[position[0]][position[1]] == self.grid[position[0]][position[1] + 1]:
+                if not alreadyFused:
+                    return 1
+                else:
+                    return 0
+
+        else:
+            return 0
 
     def movePlace(self, position, direction):
         moved = False
@@ -320,8 +400,34 @@ class Game:
             self.score = self.prev_points
         for i in range(4):
             self.grid[i] = self.previous[i].copy()
+        if self.status != 0:
+            self.status == 0
+
+    def canMoveSomething(self):
+        for i in range(4):
+            for j in range(4):
+                if self.canMove((i, j), False):
+                    return True
+        return False
+
+    def cantMoveAnything(self):
+        return not self.canMoveSomething()
+
+    def gameEnds(self):
+        for i in range(4):
+            for j in range(4):
+                if self.grid[i][j] == 2048:
+                    return 1
+        if self.cantMoveAnything():
+            return -1
+        return 0
     
     def update(self, input):
+        self.status = self.gameEnds()
+        if self.in_game and self.status:
+            self.in_game = 0
+            # s-a terminat jocul (ori am castigat, ori am pierdut), putem da undo
+
         if input == 'U':
             self.undo()
         else:
@@ -346,7 +452,15 @@ class Game:
                 elif event.key == K_RIGHT:
                     return 'S'
                 elif event.key == K_u:
+
+                    pundo_rect = pygame.Rect(418, 135, 30, 30)
+                    pundo_image = pygame.image.load("C:/Users/Felicia/python/proiect-python/images/pressed_undo.png")
+                    pundo_image = pygame.transform.scale(pundo_image, (30, 30))
+                    self.window.blit(pundo_image, pundo_rect)
+
                     return 'U'
+                #elif event.key == K_r:
+
 
 def main():
     game = Game()
